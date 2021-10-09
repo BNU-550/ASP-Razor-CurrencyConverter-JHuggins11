8d4638b2e3af8a34210213ea864b8d04113b5085
JHuggins11 <58664223+JHuggins11@users.noreply.github.com>
2021-10-09 20:53:45 +0100

    Started task 2 for logbook 1, currency converter doesn't work yet

---
+++ Razor-CurrencyConverter/Pages/SimpleCurrencyConverter.cshtml.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace Razor_CurrencyConverter.Pages
+{
+    public class SimpleCurrencyConverterModel : PageModel
+    {
+        public const double GBP_JPY_RATE = 152.74;
+
+        [BindProperty]
+        public string FullName { get; set; }
+
+        public double InputAmount { get; set; }
+        public double OutputAmount { get; set; }
+
+        private readonly ILogger<SimpleCurrencyConverterModel> _logger;
+
+        public SimpleCurrencyConverterModel
+            (ILogger<SimpleCurrencyConverterModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            FullName = "Jason Huggins";
+        }
+
+        public void OnPost()
+        {
+            if (Double.IsNaN(InputAmount))
+            {
+                ViewData["Message"] = "Invalid type. " +
+                    "The input must be a number.";
+            }
+            else if (Double.IsNegative(InputAmount))
+            {
+                ViewData["Message"] = "Invalid input. " +
+                    "The input cannot be negative.";
+            }
+            else if (InputAmount > 0)
+            {
+                OutputAmount = InputAmount * GBP_JPY_RATE;
+            }
+        }
+    }
+}
